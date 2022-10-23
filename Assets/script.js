197ed951038202398df32b9f716e828f62b94635
mgaskins17 <mattgaskins77@gmail.com>
2022-10-23 12:03:48 -0600

    Items saved to local storage for persistence

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,7 @@
 // Javascript file
-var APIkey = '40f04d918b53d1a8a149e5f84300b159'; // api key requested from openweather api
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} <- BASE URL
 
-// https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
-// https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+// Initalizing variables for API's needed for this assignment
+var APIkey = '40f04d918b53d1a8a149e5f84300b159'; // api key requested from openweather api
 var currentDate = moment().format('L');
 var currentDay = moment().format('Do');
 // Globalizing variables in case I need them later
@@ -14,13 +12,10 @@ var Cwindspd
 var Ctemp
 var Chum
 
-var CitySearch = {
-    Clat: '', 
-    Clon: '',
-    Ctemp: '',
-    Cwindspd: '',
-    Chum:'',
-};
+var CitySearch = {};
+var FiveDay = {};
+// Building function where I get the results for the current day and 5-day forecast - Do i save to local storage here?
+// What do I save to local storage? 
 function getSearchResults(cityname, APIkey) {
 
     var CityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=imperial`;
@@ -28,18 +23,24 @@ function getSearchResults(cityname, APIkey) {
     fetch(CityURL)
     .then((response) => response.json())
     .then(function(data) {
-        console.log(data);
-        CitySearch = {
-        Clat: data.coord.lat,
-        Clon: data.coord.lon,
-        Ctemp: data.main.temp,
-        Cwindspd: data.wind.speed,
-        Chum: data.main.humidity,
-        }    
-        $('#cityName').text(`${cityname} (${currentDate})`);
-        $('#cityTemp').text(`Temp: ${CitySearch.Ctemp}`);
-        $('#cityWindspd').text(`Wind Speed: ${CitySearch.Cwindspd}`);
-        $('#cityHum').text(`Humidity: ${CitySearch.Chum}`);
+            console.log(data);
+            CitySearch = {
+            CName: cityname, 
+            CDate: currentDate,
+            Clat: data.coord.lat,
+            Clon: data.coord.lon,
+            Ctemp: data.main.temp,
+            Cwindspd: data.wind.speed,
+            Chum: data.main.humidity,
+            Cweather: data.weather[0].main
+        }
+
+        localStorage.setItem('CitySearch', JSON.stringify(CitySearch));
+        console.log(CitySearch);
+        // $('#cityName').text(`${cityname} (${currentDate})`);
+        // $('#cityTemp').text(`Temp: ${CitySearch.Ctemp}`);
+        // $('#cityWindspd').text(`Wind Speed: ${CitySearch.Cwindspd}`);
+        // $('#cityHum').text(`Humidity: ${CitySearch.Chum}`);
 
         // Getting 5-day forecast using Lat/Lon saved From City Search
         var LatLonURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${CitySearch.Clat}&lon=${CitySearch.Clon}&appid=${APIkey}`;
@@ -47,26 +48,40 @@ function getSearchResults(cityname, APIkey) {
         .then((response) => response.json())
         .then(function(data) {
             console.log(data)
+            for (let i = 0; i < 5; i++) {
+                FiveDay[i] = {
+                    FiveTemp: data.list[i].main.temp,
+                    FiveWind: data.list[i].wind.speed,
+                    FiveHum: data.list[i].main.humidity,
+                    Fiveweather: data.list[i].weather[0].main
+                }
+            }
+            console.log(FiveDay);
+            localStorage.setItem('FiveDay',JSON.stringify(FiveDay));
         })
     })
 
 };
 
+
+// Create function for RenderMessage() which will take saved objects in Local Storage then display them
+function RenderCurrent() {
+    var CitySearch = JSON.parse(localStorage.getItem('CitySearch'));
+    if (CitySearch !== null) {
+        $('#cityName').text(`${CitySearch.CName} (${CitySearch.CDate})`);
+        $('#cityTemp').text(`Temp: ${CitySearch.Ctemp}`);
+        $('#cityWindspd').text(`Wind Speed: ${CitySearch.Cwindspd}`);
+        $('#cityHum').text(`Humidity: ${CitySearch.Chum}`);
+    }
+}
+
+function RenderFiveDay() {
+}
+
+RenderCurrent()
+
 $('#SearchBtn').click(function() {
     cityname = $('#CitySearch').val();
     getSearchResults(cityname, APIkey);
-})
-// getSearchResults(cityname, APIkey);
-
-
-    // 
-//     //var BaseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
-
-//     fetch(CityURL).then(function(results) {
-//         return results.json
-//     }.then((data) => console.log(data))
-// }
-
-// var results = getSearchResults(cityname, APIkey).then(function(results) {
-//     console.log(results);
-// })
+    RenderCurrent()
+});
