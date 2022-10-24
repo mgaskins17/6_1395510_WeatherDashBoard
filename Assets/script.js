2769d2bde94923a6213d5bd67cb592977ec631af
mgaskins17 <mattgaskins77@gmail.com>
2022-10-23 23:33:45 -0600

    everything works but the history buttons - used to work though

--- Assets/script.js
+++ Assets/script.js
@@ -25,13 +25,13 @@ var FiveDay = {};
 
 // Building function where I get the results for the current day and 5-day forecast - Do i save to local storage here?
 // What do I save to local storage? 
-function getSearchResults(cityname, APIkey) {
+async function getSearchResults(cityname, APIkey) {
 
     var CityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=imperial`;
     // Fetching today forecast and getting lat/Lon from the city input
-    fetch(CityURL)
+    await fetch(CityURL)
     .then((response) => response.json())
-    .then(function(data) {
+    .then(async function(data) {
             console.log(data);
             CitySearch = {
             CName: cityname, 
@@ -52,7 +52,7 @@ function getSearchResults(cityname, APIkey) {
 
         // Getting 5-day forecast using Lat/Lon saved From City Search
         var LatLonURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${CitySearch.Clat}&lon=${CitySearch.Clon}&appid=${APIkey}&units=imperial`;
-        fetch(LatLonURL)
+        await fetch(LatLonURL)
         .then((response) => response.json())
         .then(function(data) {
             console.log(data)
@@ -71,6 +71,9 @@ function getSearchResults(cityname, APIkey) {
         })
     })
 
+    RenderCurrent()
+    RenderFiveDay()
+
 };
 
 // Create function for RenderMessage() which will take saved objects in Local Storage then display them
@@ -87,31 +90,17 @@ function RenderCurrent() {
 function RenderFiveDay() {
     var FiveObj = JSON.parse(localStorage.getItem('FiveDay')); // tried using FiveDay but it created issues - don't reuse global variables for local purposes
     for (let i = 0; i < 5; i++) {
-        ResetCards(i);
-    }
-    for (let i = 0; i < 5; i++) {
+        console.log('render five works')
+        $(`#Day${i}`).empty()
         $('<div>').addClass('added').text(`${currentMonth}/${parseInt(currentDay)+i+1}/${currentYear}`).attr('style','font-size: 1rem; font-weight: bold').appendTo(`#Day${i}`)
         $('<div>').addClass('added').text(`Temp: ${FiveObj[i].FiveTemp} °F`).attr('style','font-size: .9rem').appendTo(`#Day${i}`)
         Temppic = $('<img>').addClass('added').attr('src',`https://openweathermap.org/img/wn/${FiveObj[i].FiveIcon}.png`);
-        Temppic.attr('style','height:25%; width:25%').appendTo(`#Day${i}`)
+        Temppic.attr('style','height:25%; width:40%; margin:0 auto').appendTo(`#Day${i}`)
         $('<div>').addClass('added').text(`Wind Speed: ${FiveObj[i].FiveWind} mph`).attr('style','font-size: .9rem').appendTo(`#Day${i}`)
         $('<div>').addClass('added').text(`Humidity: ${FiveObj[i].FiveHum}`).attr('style','font-size: .9rem').appendTo(`#Day${i}`)
     }
 }
 
-function ResetCards(i) {
-    while (`Day${i}`.children) { // Before appending the other answer choices, this will remove all children until none are left
-        (`Day${i}`).empty()
-    }
-}
-
-
-// Checking to see if there is something in local storage then outputting if there is
-if (localStorage.getItem('CitySearch') !== null) {
-    RenderCurrent();
-    RenderFiveDay();
-    SearchRender();
-}
 
 // Search button action of putting out new results
 $('#SearchBtn').click(function() {
@@ -121,32 +110,27 @@ $('#SearchBtn').click(function() {
        localStorage.setItem('Searches',JSON.stringify(SearchHistory));
     }
     getSearchResults(cityname, APIkey);
-    RenderCurrent()
-    RenderFiveDay()
     SearchRender()
 });
 
 $('#ClearBtn').click(function() {
     SearchHistory = [];
     localStorage.setItem('Searches',JSON.stringify(SearchHistory));
-    $('#past').empty()
-})
-
-// Reacting to when a past history button is pressed
-$('.HistSearch-btn').click(function() {
-    cityname = $(this).text();
-    console.log(cityname);
-    getSearchResults(cityname, APIkey);
-    RenderCurrent()
-    RenderFiveDay()
+    $('#past').empty
     SearchRender()
 })
 
-// function resetSearch() {
-//     while ($('#past').children) { // Before appending the other answer choices, this will remove all children until none are left
-//         $('#past').empty()
-//     }
-// }
+// Reacting to when a past history button is pressed
+$(".Hist-btn").on('click', function() {
+    console.log('please work now');
+})
+
+// Checking to see if there is something in local storage then outputting if there is
+if (localStorage.getItem('CitySearch') !== null) {
+    Recover = JSON.parse(localStorage.getItem('CitySearch'))
+    getSearchResults(Recover.CName,APIkey)
+    SearchRender()
+}
 
 // Build Search History 
 // Treat each one as a button that will output that cities weather forecast
@@ -156,9 +140,8 @@ function SearchRender() {
     if (localStorage.getItem('Searches') !== null) {
     var SearchHis = JSON.parse(localStorage.getItem('Searches'));
     }
-
     for (let i = 0; i<SearchHis.length; i++) {
-        $('<button>').text(SearchHis[i]).addClass('btn HistSearch-btn btn-danger d-block p-2 my-2 form-control').appendTo('#past');
+        $('<div>').text(SearchHis[i]).addClass('Hist-btn btn btn-danger d-block p-2 my-2 form-control').appendTo('#past');
     }
 }
 
